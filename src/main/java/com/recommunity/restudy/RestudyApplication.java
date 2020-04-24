2268e349a5b1bd53f2b52d09313d140379214e8d
zichouyinmao <1767310818@qq.com>
2020-04-24 08:58:45 +0800

    init

---
+++ src/main/java/com/recommunity/restudy/RestudyApplication.java
@@ -0,0 +1,13 @@
+package com.recommunity.restudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/recommunity/restudy/RestudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.recommunity.restudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
